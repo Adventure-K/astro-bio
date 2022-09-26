60a191826d5854fbc43d3251fc2cbee8b5e856be
Andrew Kane <andrew.kane@gmail.com>
2022-09-25 19:51:09 -0500

    initial commit

---
+++ src/links.js
@@ -0,0 +1,44 @@
+export const mainContent = {
+  imgSrc: `/assets/profile.png`,
+  name: `Andrew Kane`,
+  tagline: `Welcome to my Agonizing Hellsite!`
+};
+
+export const socialLinks = [
+
+  {
+    title: 'LinkedIn',
+    icon: 'mdi:linkedin  ',
+    url: 'https://www.linkedin.com/in/andrew-b-kane/',
+  },
+  {
+    title: 'GitHub',
+    icon: 'mdi:github',
+    url: 'https://github.com/Adventure-K',
+  },
+  {
+    title: 'Facebook',
+    icon: 'mdi:facebook',
+    url: 'https://www.facebook.com/andrew.kane.1422',
+  }
+]
+
+export const links = [
+  {
+    text: 'Home',
+    url: 'https://littlesticks.dev',
+    featured: true,
+  },
+  {
+    text: 'About',
+    url: 'https://jaydanurwin.com/about',
+  },
+  {
+    text: 'Uses',
+    url: 'https://jaydanurwin.com/uses',
+  },
+  {
+    text: 'See my latest blog post!',
+    url: 'https://jaydanurwin.com/about',
+  }
+]
