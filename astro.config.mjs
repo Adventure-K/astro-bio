94b688e22aac126d495fce8308a6d453bc5b4735
Andrew Kane <andrew.kane@gmail.com>
2022-09-25 20:19:37 -0500

    setup for github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://adventure-k.github.io',
+  base: '/astro-bio',
   vite: {
     ssr: {
       external: ["svgo"],
